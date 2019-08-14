23d1d7242a9237986300873b6045edbfca229e95
Darrow Palast <dpalast@gmail.com>
2019-08-14 21:22:31 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -44,7 +44,10 @@ end
 
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  "SELECT "
+  "SELECT 
+  FROM projects
+  JOIN pledges ON projects.id = pledges.project_id
+  "
 end
 
 
